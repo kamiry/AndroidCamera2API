95f5ec01ad9cd8885a314be63f2e08df899ead7a
jcliu <jcliu@ncnu.edu.tw>
2017-09-18 16:51:58 +0800

    update

--- app/src/main/java/com/example/jcliu/androidcamera2api/MainActivity.java
+++ app/src/main/java/com/example/jcliu/androidcamera2api/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.jcliu.androidcamera2api;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.TextureView;
+import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
@@ -20,5 +21,13 @@ public class MainActivity extends AppCompatActivity {
         //textureView.setSurfaceTextureListener(textureListener);
         //
         takePictureBtn = (Button) findViewById(R.id.btn_takepicture);
+        assert takePictureBtn != null;
+        takePictureBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //takePicture();
+            }
+        });
     }
+
 }
