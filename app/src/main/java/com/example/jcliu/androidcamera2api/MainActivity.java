c51084e8eec6b2bd61441da03a92c8e8b57d6cea
jcliu <jcliu@ncnu.edu.tw>
2017-09-18 16:35:50 +0800

    Initial commit

---
+++ app/src/main/java/com/example/jcliu/androidcamera2api/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.jcliu.androidcamera2api;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.TextureView;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextureView textureView;
+    private Button takePictureBtn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //
+        textureView = (TextureView) findViewById(R.id.textureView);
+        assert textureView != null;
+        //textureView.setSurfaceTextureListener(textureListener);
+        //
+        takePictureBtn = (Button) findViewById(R.id.btn_takepicture);
+    }
+}
